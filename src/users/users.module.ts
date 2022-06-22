7283469f69001dd330e2a764b828550ee5800db0
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-06-22 13:09:28 -0300

    import module mongoose

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://Oriental222:5xW4z9Zcu-QD-Vt@cluster0.ynhjkvu.mongodb.net/?retryWrites=true&w=majority')
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
