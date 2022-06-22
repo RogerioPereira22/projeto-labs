f7ab2eec95a91f3ef9fc5a5af86309425d1fa4b7
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-06-22 16:39:02 -0300

    delete crud local

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://Oriental222:5xW4z9Zcu-QD-Vt@cluster0.ynhjkvu.mongodb.net/?retryWrites=true&w=majority'),
-    UsersModule, TasksModule],
+    MongooseModule.forRoot(
+      'mongodb+srv://Oriental222:5xW4z9Zcu-QD-Vt@cluster0.ynhjkvu.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -4,25 +4,23 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 @Injectable()
 export class TaskService {
-    constructor(@InjectModel('Task')private readonly taskModel:Model<Task>){
-      
-    }
+  constructor(@InjectModel('Task') private readonly taskModel: Model<Task>) {}
   async getAll() {
-   return await this.taskModel.find().exec();
+    return await this.taskModel.find().exec();
   }
   async getById(id: string) {
     return await this.taskModel.findById(id).exec();
   }
   async create(task: Task) {
-   const createdTask = new this.taskModel(task);
-   return await createdTask.save();
+    const createdTask = new this.taskModel(task);
+    return await createdTask.save();
   }
-  async update(id:string, task:Task) {
-     await this.taskModel.updateOne({_id: id},task).exec()
-     return this.getById(id);
-    }
-  
+  async update(id: string, task: Task) {
+    await this.taskModel.updateOne({ _id: id }, task).exec();
+    return this.getById(id);
+  }
+
   async delete(id: string) {
-    return await this.taskModel.deleteOne({_id: id }).exec();
+    return await this.taskModel.deleteOne({ _id: id }).exec();
   }
 }
