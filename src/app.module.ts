283f8b8165285e0986b6fe7455e823f1ad539b97
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-06-23 14:30:12 -0300

    criand schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './users/entities/user.entity';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://Oriental222:5xW4z9Zcu-QD-Vt@cluster0.ynhjkvu.mongodb.net/?retryWrites=true&w=majority',
     ),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,20 @@
+import { Prop,Schema,SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type UserDocument = User & Document;
+@Schema()
 export class User {
+  @Prop()
   id: number;
+  @Prop()
   name: string;
+  @Prop()
   email: string;
+  @Prop()
   password: string;
+  @Prop()
   reserva:string;
+  @Prop()
   hotel:string;
 }
+ export const UserSchema = SchemaFactory.createForClass(User);
