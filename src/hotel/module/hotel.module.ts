2d340c07f6f44f5ff23fa1d24cd5be12b973bbb6
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-06-28 14:16:56 -0300

    delete imports

--- src/hotel/module/hotel.module.ts
+++ src/hotel/module/hotel.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HotelService } from '../service/hotel.service';
 import { HotelController } from '../controller/hotel.controller';
-import { RouterModule } from '@nestjs/core';
-import { ReservaModule } from 'src/reserva/module/reserva.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [
-    RouterModule.register([
-      {
-        path: 'hotel',
-        module: HotelModule,
-        children: [
-          {
-            path: 'reserva',
-            module: ReservaModule,
-          },
-        ],
-      },
-    ]),
-  ],
+  imports: [DatabaseModule],
   controllers: [HotelController],
   providers: [HotelService],
 })
