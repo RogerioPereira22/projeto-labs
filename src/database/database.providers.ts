21d4a588c142ca7712ce7cf5d82825a3e92eedb9
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-06-29 14:56:16 -0300

    atulizei endereco banco

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
     {
         provide :'DATABASE_CONNECTION',
         useFactory: (): Promise<typeof mongoose> =>
-        mongoose.connect('mongodb+srv://Oriental222:5xW4z9Zcu-QD-Vt@cluster0.ynhjkvu.mongodb.net/?retryWrites=true&w=majority'),
+        mongoose.connect('mongodb+srv://Oriental222:5xW4z9Zcu-QD-Vt@cluster0.vu6dj4j.mongodb.net/?retryWrites=true&w=majority'),
         },
 ];
