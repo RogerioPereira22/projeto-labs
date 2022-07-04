85826d263f99f9b5cf2791f83a91a90c11070fe5
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-07-04 18:17:54 -0300

    update reserva schema

--- src/reserva/schema/reserva.schema.ts
+++ src/reserva/schema/reserva.schema.ts
@@ -1,5 +1,48 @@
-import * as mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { plainToInstance } from 'class-transformer';
+import mongoose, { Document } from 'mongoose';
+import { ReservasDataObject } from '../objects'; 
 
-export const ReservaSchema = new mongoose.Schema({
-  reserva: String,
+export type BookingDocument = Reserva &
+  Document & {
+    plainToInstance: () => ReservasDataObject;
+    timestamp: Date;
+  };
+
+@Schema()
+export class Reserva {
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel',
+  })
+  hotel: string;
+
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Guest',
+  })
+  guest: string;
+
+  @Prop({ type: Date, required: true })
+  checkIn: Date;
+
+  @Prop({ type: Date, required: true })
+  checkOut: Date;
+
+  @Prop({
+    required: true,
+  })
+  amount: number;
+}
+
+export const ReservaSchema = SchemaFactory.createForClass(Reserva);
+
+ReservaSchema.index({ hotel: 1 });
+ReservaSchema.index({ checkIn: 1 });
+ReservaSchema.index({ checkOut: 1 });
+
+ReservaSchema.method('plainToInstance', function () {
+  return plainToInstance(ReservasDataObject, this.toObject());
 });
