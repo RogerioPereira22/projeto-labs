c2982c18af68093f495a6bde54d8b908d321453c
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-07-04 18:21:23 -0300

    add shema no mogo

--- src/reserva/module/reserva.module.ts
+++ src/reserva/module/reserva.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReservaService } from '../service/reserva.service';
-import { ReservaController } from '../controller/reserva.controller'; 
+import { ReservaController } from '../controller/reserva.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { ReservaProviders } from '../providers/reserva.providers';
+import { Reserva, ReservaSchema } from '../schema/reserva.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    MongooseModule.forFeature([{ name: Reserva.name, schema: ReservaSchema }]),
+  ],
   controllers: [ReservaController],
-  providers: [ReservaService,...ReservaProviders],
+  providers: [ReservaService, ...ReservaProviders],
 })
 export class ReservaModule {}
