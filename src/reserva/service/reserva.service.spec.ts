73f6f1d7ecb2627cbdc210ac1ec531e0a527c809
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-07-04 18:25:30 -0300

    teste reserva

--- src/reserva/service/reserva.service.spec.ts
+++ src/reserva/service/reserva.service.spec.ts
@@ -1,12 +1,22 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConfigurationKeys } from 'src/config/configuration.keys';
+import { Types } from 'mongoose';
+import { rootMongooseTestModule } from '../root-mongo-test.module';
 import { ReservaService } from './reserva.service';
+import { Reserva,ReservaSchema } from '../schema/reserva.schema'; 
+import * as dayjs from 'dayjs';
 
-describe(ConfigurationKeys.ReservaService, () => {
+describe('ReservaService', () => {
   let service: ReservaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([
+          { name: Reserva.name, schema: ReservaSchema },
+        ]),
+      ],
       providers: [ReservaService],
     }).compile();
 
@@ -15,5 +25,32 @@ describe(ConfigurationKeys.ReservaService, () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create booking', async () => {
+    const bookingInfo = {
+      hotel: new Types.ObjectId().toHexString(),
+      guest: new Types.ObjectId().toHexString(),
+      checkIn: dayjs().add(1, 'day').toDate(),
+      checkOut: dayjs().add(2, 'day').toDate(),
+      amount: 100,
+    };
+    const booking = await service.create(bookingInfo);
+    expect(booking).toEqual(expect.objectContaining(bookingInfo));
+  });
+
+  it('should allow only 10 booking per day', async () => {
+    const bookingInfo = {
+      hotel: new Types.ObjectId().toHexString(),
+      guest: new Types.ObjectId().toHexString(),
+      checkIn: dayjs().add(1, 'day').toDate(),
+      checkOut: dayjs().add(2, 'day').toDate(),
+      amount: 100,
+    };
+    const bookings = await Promise.all(
+      [...Array(10)].map(async () => service.create(bookingInfo)),
+    );
+    expect(bookings.length).toBe(10);
+    expect(service.create(bookingInfo)).rejects.toThrow();
   });
 });
