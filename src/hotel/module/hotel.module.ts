9405a558a09f7b9395d058fe2d042f91c62d4bbd
oriental222 <79112095+oriental222@users.noreply.github.com>
2022-07-05 11:28:48 -0300

    removendo funcao app desnecessarias

--- src/hotel/module/hotel.module.ts
+++ src/hotel/module/hotel.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { HotelService } from '../service/hotel.service';
 import { HotelController } from '../controller/hotel.controller';
-import { DatabaseModule } from 'src/database/database.module';
 import { GoogleMapsModule } from 'src/config/google-maps';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReservaModule } from 'src/reserva';
+import { UsersModule } from 'src/users';
+import { Hotel, HotelSchema } from '../schema/hotel.schema';
 
 @Module({
-  imports: [imports: [
+  imports: [
     MongooseModule.forFeature([{ name: Hotel.name, schema: HotelSchema }]),
     GoogleMapsModule,
-    ReservasModule,
-    GuestModule,
-  ],],
+    ReservaModule,
+    UsersModule,
+  ],
   controllers: [HotelController],
   providers: [HotelService],
 })
--- src/reserva/module/reserva.module.ts
+++ src/reserva/module/reserva.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ReservaService } from '../service/reserva.service';
-import { ReservaController } from '../controller/reserva.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { ReservaProviders } from '../providers/reserva.providers';
 import { Reserva, ReservaSchema } from '../schema/reserva.schema';
@@ -11,7 +10,6 @@ import { MongooseModule } from '@nestjs/mongoose';
     DatabaseModule,
     MongooseModule.forFeature([{ name: Reserva.name, schema: ReservaSchema }]),
   ],
-  controllers: [ReservaController],
   providers: [ReservaService, ...ReservaProviders],
 })
 export class ReservaModule {}
